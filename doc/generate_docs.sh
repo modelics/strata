0c6395edb342700b2e6abaf9e65ed07cb278730a
Shashwat Sharma <shash.sharma@mail.utoronto.ca>
2021-11-07 01:24:48 -0400

    working on documentation

--- doc/generate_docs.sh
+++ doc/generate_docs.sh
@@ -6,8 +6,16 @@
 # Prerequisites:
 # latex, latexmk, sphinx
 
-make clean
-make html
-make latexpdf
-cp ./build/latex/Strata.pdf ./
-ln -sf ./build/html/contents.html ./Strata.html
+# make clean
+# make html
+# make latexpdf
+# cp ./build/latex/Strata.pdf ./
+# ln -sf ./build/html/contents.html ./Strata.html
+
+sphinx-build -b html ./source ./build/html
+sphinx-build -b latex ./source ./build/latex
+make -C ./build/latex
+cp ./build/latex/strata.pdf ./
+ln -sf ./build/html/index.html ./strata.html
+
+
